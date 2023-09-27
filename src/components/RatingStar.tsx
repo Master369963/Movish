d65d65bf0edc0f47cc335ab149b8b72f944aa492
Agatha <master369_@hotmail.com>
2023-09-27 15:26:56 +0100

    modify star color at light mode

--- src/components/RatingStar.tsx
+++ src/components/RatingStar.tsx
@@ -11,12 +11,18 @@ const RatingStar = ({ rating }: { rating: number }) => {
     for (let i = 0; i < maxStars; i++) {
       if (i < fullStars) {
         stars.push(
-          <PiStarFill key={i} className="text-xl text-secondary-100" />,
+          <PiStarFill
+            key={i}
+            className="text-xl text-secondary-200 dark:text-secondary-100"
+          />,
         );
         // full star
       } else if (i === fullStars && halfStar) {
         stars.push(
-          <PiStarHalfFill key={i} className="text-xl text-secondary-100" />,
+          <PiStarHalfFill
+            key={i}
+            className="text-xl text-secondary-200 dark:text-secondary-100"
+          />,
         );
         // half star
       }
